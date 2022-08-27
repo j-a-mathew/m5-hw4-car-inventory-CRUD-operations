3fa3f31e37a6f7cc05680d81bfd5c18c96d6dbc3
Justin Mathew <jamathew38@gmail.com>
2022-08-27 11:54:53 -0400

    first commit

---
+++ app/site/routes.py
@@ -0,0 +1,8 @@
+from re import template
+from flask import Blueprint, render_template
+
+site = Blueprint('site', __name__, template_folder='site_templates')
+
+@site.route('/')
+def home():
+    return render_template('index.html')
